d0620d6dda01eaf9cf5748f83ff24dae0f00b9d1
Mohammed Saleh <mohammedsalehbtw@gmail.com>
2024-05-08 20:38:23 +0300

    Module files

---
+++ \system\media/CherishOS/customize.sh
@@ -0,0 +1,2 @@
+ui_print "Change Bootanimation for devices have /system/media/bootanimation.zip"
+ui_print "Finished!! enjoy!"
---
+++ \system\product\media/google/customize.sh
@@ -0,0 +1,2 @@
+ui_print "Change Bootanimation for devices have /system/product/media/bootanimation.zip"
+ui_print "Finished!! enjoy!"
